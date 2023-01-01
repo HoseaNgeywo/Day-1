8ebe9769e33eb3a685890a3722beafc8cfa299cf
HoseaNgeywo <hiltonngeywo@gmail.com>
2023-01-01 22:12:50 +0300

    Python string manipulation

--- Day1.py
+++ Day1.py
@@ -6,3 +6,36 @@ print(random.randrange(1, 9))
 
 j = "Happy new year 2023"
 print(j[6])
+
+# looping through strings
+
+for x in "2023":
+    print(x)
+
+
+# Check string len
+b = 'New Year'
+print(len(b))
+
+
+#Locate a given word in a string
+
+year = 'The best offer of the year with free addons'
+print('uth' in year)
+
+
+if "free" in year:
+    print("Yes, 'mk' offers available.")
+else:
+    print('No free offers')
+    
+# if not
+
+y = 'Annual presidential scholarship is awarded to exellent students.'
+print('Exellent' not in y)
+
+
+if 'Exellent' not in y:
+    print('No, exellent NOT present')
+else:
+    print('Yes, Exellent present')
